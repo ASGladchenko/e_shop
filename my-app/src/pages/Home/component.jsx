7ca979a21b95a6c71659c6a81994da6452df3d91
ASGladchenko <asgladchenko@gmail.com>
2023-01-13 12:39:14 +0200

    add route(just structure): Home, BadGateWay(commented out); pages(empty): Home; components(empty): Header, Footer; connected bootstrap styles through main.scss

---
+++ my-app/src/pages/Home/component.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+import './styles.scss'
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
